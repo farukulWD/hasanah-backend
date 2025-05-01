35cee3c863e5b351ebac35f6acd9c55fceabf871
farukulWD <farukgb1999@gmail.com>
2025-05-02 00:59:53 +0600

    added email sender and cloudinay configure

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,10 +9,14 @@ const envPath =
 dotenv.config({ path: envPath });
 
 export default {
+  NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3000,
   DB_URL: process.env.DB_URL || "mongodb://localhost:27017/myapp",
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   JWT_EXPIRATION: process.env.JWT_EXPIRATION,
   JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION,
+  MULTER_CLOUD_NAME: process.env.MULTER_CLOUD_NAME,
+  MULTER_API_KEY: process.env.MULTER_API_KEY,
+  MULTER_API_SECRET: process.env.MULTER_API_SECRET,
 };
